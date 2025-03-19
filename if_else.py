a519b8a3e40a26d5c008955b5b74d1d72f6895d3
unknown <AdminSena@BOGDFPCGMF4144.FormacionSena.RED>
2025-03-19 07:47:41 -0500

    if_else

---
+++ if_else.py
@@ -0,0 +1,24 @@
+'''
+
+Una condicional y determina dos caminos de ejecucion
+
+if condicional:
+    inst_1
+    inst_2
+else
+    inst_3
+    inst_4
+
+'''
+
+n = 17
+
+if n < 18:
+
+    print("Usted no puede votar")
+
+else:
+
+    print("Usted puede votar")
+
+print("Fin")
